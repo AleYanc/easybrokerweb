ab48e533955e48edf6a6151b45fe0766c5e24c13
AleYanc <aleyanc2000@gmail.com>
2021-12-20 12:54:06 -0300

    Filled index action with http request to EasyBroker API (properties endpoint)

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,11 +1,20 @@
 class PropertiesController < ApplicationController
+  BASE_URL='https://api.stagingeb.com/v1/properties'
+  HEADERS = { 
+    "X-Authorization"  => ENV['API_KEY']
+  }
 
   def index
-
+    current_page = params[:page]
+    query = {
+      "limit" => "15",
+      "page" => current_page || 1
+    }
+    request = HTTParty.get(BASE_URL, :query => query, :headers => HEADERS)
+    @response = JSON.parse(request.body)
   end
 
   def show
-
   end
   
 end
