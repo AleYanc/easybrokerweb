5567309fe7050a0a16a80deec9cc89ee342901d5
AleYanc <aleyanc2000@gmail.com>
2021-12-20 18:24:19 -0300

    Added post_contact method to make new leads

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,22 +1,45 @@
 class PropertiesController < ApplicationController
-  BASE_URL='https://api.stagingeb.com/v1/properties'
-  HEADERS = { 
+  BASE_URL='https://api.stagingeb.com/v1/'
+  HEADERS = {
+    "Content-Type" => 'application/json',
+    'charset' => 'utf-8', 
     "X-Authorization"  => ENV['API_KEY']
   }
-
-  def index
-    @current_page = [1, params[:page].to_i].max
-    query = {
+  QUERY = {
       "limit" => "15",
       "page" => @current_page
     }
-    request = HTTParty.get(BASE_URL, :query => query, :headers => HEADERS)
+
+  def index
+    @current_page = [1, params[:page].to_i].max
+    request = HTTParty.get(BASE_URL + 'properties', :query => QUERY, :headers => HEADERS)
     @response = JSON.parse(request.body)
   end
 
   def show
-    request = HTTParty.get(BASE_URL + "/#{params[:id]}", :headers => HEADERS)
+    request = HTTParty.get(BASE_URL + "properties/#{params[:id]}", :headers => HEADERS)
     @response = JSON.parse(request.body)
+  end
+
+  def post_contact
+    data = {
+      name: params[:name],
+      phone: params[:phone],
+      email: params[:email],
+      property_id: params[:property_id],
+      message: params[:message],
+      source: "mydomain.com"
+    }
+
+    @response = HTTParty.post(BASE_URL + 'contact_requests', {body: data.to_json, :headers => HEADERS })
+
+    if @response["status"] == "successful"
+      flash[:notice] = "¡Mensaje enviado! Estaremos en contacto pronto"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:danger] = @response["error"]
+      redirect_back(fallback_location: root_path)
+    end
   end
   
 end
