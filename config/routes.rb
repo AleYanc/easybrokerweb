f02d1de116edefb81500b5af34c7e8387f8b9013
AleYanc <aleyanc2000@gmail.com>
2021-12-20 18:26:52 -0300

    Added route to post contact request

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'properties#index'
-  get 'properties/:id', to: 'properties#show'
+  resources :properties, only: [:show]
+  post 'post_contact', to: 'properties#post_contact'
 end
